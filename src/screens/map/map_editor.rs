fa3867716d138701b1f177384f0e625b0549a7d6
Dan Stoian <danmihaistoian@gmail.com>
2026-06-02 09:59:15 +0300

    correct camera zoom

--- src/screens/map/map_editor.rs
+++ src/screens/map/map_editor.rs
@@ -46,6 +46,7 @@ impl MapEditor {
 #[derive(Default)]
 pub struct CanvasState {
     left_pressed_at: Option<Point>,
+    current_drag_position: Option<Point>,
     camera: Camera,
 }
 
@@ -62,9 +63,40 @@ impl canvas::Program<Message> for MapEditor {
     ) -> Vec<canvas::Geometry> {
         let mut frame = canvas::Frame::new(renderer, bounds.size());
         let zoom = _state.camera.zoom;
-        for grave in &self.graves {
 
-            let grave_to_screen = _state.camera.world_to_screen(Point::new(grave.coordinate.top_left_x(), grave.coordinate.top_left_y()));
+        if let Some(current_drag) = _state.current_drag_position {
+            let ghost_grave: Grave =
+                (_state.left_pressed_at.unwrap_or(current_drag), current_drag).into();
+            let ghost_to_screen = _state.camera.world_to_screen(Point::new(
+                ghost_grave.coordinate.top_left_x(),
+                ghost_grave.coordinate.top_left_y(),
+            ));
+            let rect = iced::Rectangle {
+                x: ghost_to_screen.x,
+                y: ghost_to_screen.y,
+                width: ghost_grave.coordinate.width() * zoom,
+                height: ghost_grave.coordinate.height() * zoom,
+            };
+            let path = canvas::Path::rectangle(rect.position(), rect.size());
+
+            frame.stroke(
+                &path,
+                canvas::Stroke {
+                    width: 2.0,
+                    style: canvas::Style::Solid(iced::Color::WHITE),
+                    line_dash: canvas::LineDash {
+                        segments: &[6.0, 4.0],
+                        offset: 0,
+                    },
+                    ..Default::default()
+                },
+            );
+        }
+        for grave in &self.graves {
+            let grave_to_screen = _state.camera.world_to_screen(Point::new(
+                grave.coordinate.top_left_x(),
+                grave.coordinate.top_left_y(),
+            ));
             let rect = iced::Rectangle {
                 x: grave_to_screen.x,
                 y: grave_to_screen.y,
@@ -97,6 +129,7 @@ impl canvas::Program<Message> for MapEditor {
                 None
             }
             iced::Event::Mouse(iced::mouse::Event::ButtonReleased(iced::mouse::Button::Left)) => {
+                _state.current_drag_position = None;
                 let cursor = _cursor.position_in(_bounds)?;
 
                 let p1 = _state.left_pressed_at.take()?;
@@ -108,12 +141,25 @@ impl canvas::Program<Message> for MapEditor {
                 }
                 None
             }
+            iced::Event::Mouse(iced::mouse::Event::CursorMoved { position }) => {
+                if let Some(starting_point) = _state.left_pressed_at {
+                    let current_position_to_world = _state.camera.screen_to_world(*position);
+                    _state.current_drag_position = Some(current_position_to_world);
+                    return Some(Action::request_redraw());
+                }
+                None
+            }
             iced::Event::Mouse(iced::mouse::Event::WheelScrolled { delta }) => {
+                let cursor = _cursor.position_in(_bounds)?;
+                let before_zoom_cursor = _state.camera.screen_to_world(cursor);
                 let zoom_amount = match delta {
                     iced::mouse::ScrollDelta::Lines { y, .. } => *y as f32 * 0.1,
                     iced::mouse::ScrollDelta::Pixels { y, .. } => *y as f32 * 0.001,
                 };
                 _state.camera.zoom = (_state.camera.zoom + zoom_amount).clamp(0.1, 10.0);
+                let after_zoom_curor = _state.camera.screen_to_world(cursor);
+                _state.camera.offset.x += before_zoom_cursor.x - after_zoom_curor.x;
+                _state.camera.offset.y += before_zoom_cursor.y - after_zoom_curor.y;
                 return Some(Action::request_redraw());
             }
             _ => None,
